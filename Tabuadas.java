1384dd0538e828e856746b02904b0db3885e0691
GabrielOrtega23 <ortegafaiagabriel@gmail.com>
2025-09-10 15:04:14 -0300

    Add files via upload

---
+++ Tabuadas.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tabuadas;
+import java.util.Scanner;
+/**
+ *
+ * @author FATEC ZONA LESTE
+ */
+public class Tabuadas {
+
+    public static void main(String[] args) {
+       // Criar o objeto Scanner para ler o número digitado pelo usuário
+        Scanner sc = new Scanner(System.in);
+
+        // Pedir para o usuário informar o número da tabuada
+        System.out.print("Digite um número para ver a tabuada: ");
+        int numero = sc.nextInt(); // Lê o número digitado
+
+        // Variável auxiliar para multiplicar (vai de 1 até 10)
+        int i = 1;
+
+        // Estrutura de repetição do...while
+        System.out.println("\nTabuada do " + numero + ":");
+        do {
+            // Exibir o cálculo na tela
+            System.out.println(numero + " x " + i + " = " + (numero * i));
+
+            // Incrementa i para passar ao próximo cálculo
+            i++;
+        } while (i <= 10); // Repete enquanto i for menor ou igual a 10
+
+        // Fechar o Scanner
+        sc.close();
+    }
+}
+    
+
